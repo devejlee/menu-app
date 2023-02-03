6c527b7ef0cfcb5d773986da99da8acf735e38f5
Elijah Lee <develijahlee@gmail.com>
2023-02-03 20:18:52 +0900

    add NotFound page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import StepThree from './components/StepThree/StepThree'
 import StepTwo from './components/StepTwo/StepTwo'
 import StepFour from './components/StepFour/StepFour'
 import Navigation from './components/Navigation/Navigation'
+import NotFound from './components/NotFound/NotFound'
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route path='/step-two' element={<StepTwo />} />
         <Route path='/step-three' element={<StepThree />} />
         <Route path='/step-four' element={<StepFour />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </>
   )
