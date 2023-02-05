136f723485a83bb64b1512e548f9f1965a139110
Elijah Lee <develijahlee@gmail.com>
2023-02-05 20:23:03 +0900

    hamdle dish not selected error

--- src/components/StepThree/StepThree.tsx
+++ src/components/StepThree/StepThree.tsx
@@ -1,20 +1,34 @@
 import DishWithServing from '../DishWithServing/DishWithServing';
 import { useDishesStore } from '../../store/dishesStore';
 import PreviousButton from '../PreviousButton/PreviousButton';
+import { useState, useEffect } from 'react';
 
 const StepThree = () => {
   const selectedDishes = useDishesStore(state => state.selectedDishes);
+  const selectedDish = useDishesStore(state => state.selectedDish);
   const dishesFilteredByRestaurants = useDishesStore(state => state.dishesFilteredByRestaurants);
   const dishesFilteredBySelectedDishes = dishesFilteredByRestaurants.filter(dish => !selectedDishes.some(selectedDish => dish.id === selectedDish.id));
   const addSelectedDishes = useDishesStore(state => state.addSelectedDishes);
   const resetSelectedDish = useDishesStore(state => state.resetSelectedDish);
   const resetSelectedServings = useDishesStore(state => state.resetSelectedServings);
 
+  const [error, setError] = useState(false);
+
   const handleClick = () => {
+    if (selectedDish.id === null) {
+      setError(true);
+      return;
+    }
     addSelectedDishes();
     resetSelectedDish();
     resetSelectedServings();
   };
+
+  useEffect(() => {
+    if (selectedDish.id !== null) {
+      setError(false);
+    }
+  }, [selectedDish.id]);
 
   return (
     <main className='mt-4'>
@@ -24,6 +38,9 @@ const StepThree = () => {
       <PreviousButton url='/step-two' />
       {dishesFilteredBySelectedDishes.length > 0 && (
         <button className='mt-2 ml-4 rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900' onClick={handleClick}>Add Dish</button>
+      )}
+      {error && (
+        <p className='mt-4 text-red-500'>Dish not selected</p>
       )}
     </main>
   );
